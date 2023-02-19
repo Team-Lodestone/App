e41534b3edfaa42582f69fa27c43abd6ad45d90a
Tristan F <LeoDog896@hotmail.com>
2023-02-19 18:59:48 -0500

    refactor: move to TS & move paths

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
 
 <div class="row">
   <a href="https://vitejs.dev" target="_blank">
-    <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+    <img src="vite.svg" class="logo vite" alt="Vite Logo" />
   </a>
   <a href="https://tauri.app" target="_blank">
-    <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+    <img src="tauri.svg" class="logo tauri" alt="Tauri Logo" />
   </a>
   <a href="https://kit.svelte.dev" target="_blank">
-    <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+    <img src="svelte.svg" class="logo svelte" alt="Svelte Logo" />
   </a>
 </div>
 

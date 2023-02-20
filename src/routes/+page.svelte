f43223b9240c05fdfe9af24b70b806b256a0e4e2
Tristan F <LeoDog896@hotmail.com>
2023-02-19 19:04:53 -0500

    fix: URLs by direct import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,22 @@
 <script>
   import Greet from "$lib/Greet.svelte";
+
+  import Vite from "$lib/vite.svg"
+  import Tauri from "$lib/tauri.svg"
+  import Svelte from "$lib/svelte.svg"
 </script>
 
 <h1>Welcome to Tauri!</h1>
 
 <div class="row">
-  <a href="https://vitejs.dev" target="_blank">
-    <img src="vite.svg" class="logo vite" alt="Vite Logo" />
+  <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+    <img src={Vite} class="logo vite" alt="Vite Logo" />
   </a>
-  <a href="https://tauri.app" target="_blank">
-    <img src="tauri.svg" class="logo tauri" alt="Tauri Logo" />
+  <a href="https://tauri.app" target="_blank" rel="noreferrer">
+    <img src={Tauri} class="logo tauri" alt="Tauri Logo" />
   </a>
-  <a href="https://kit.svelte.dev" target="_blank">
-    <img src="svelte.svg" class="logo svelte" alt="Svelte Logo" />
+  <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">
+    <img src={Svelte} class="logo svelte" alt="Svelte Logo" />
   </a>
 </div>
 

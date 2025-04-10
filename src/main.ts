fc560df354d6d8fabc1a5c5419f96294cd81445a
Dexrn ZacAttack <dexrn@hotmail.com>
2025-04-10 16:42:05 -0700

    fixed

--- src/main.ts
+++ src/main.ts
@@ -12,24 +12,28 @@ export { version } from '../package.json'
 
 let app: { $set?: never; $on?: never } | undefined = undefined
 
+// not sure how Awaited works but it looks like already fulfilled Promise or something?
+export let libLCE: Awaited<ReturnType<typeof LibLCEWasm>>
+
 async function initApp(): Promise<Record<never, never>> {
   await initL10N()
 
   // Vite is being really dumb on my machine, so this fixes double mounting the App bc it throws if the element doesn't exist
   document.getElementById('unsupportedMessage').remove()
+  // @ts-ignore Because it already IS esnext.
+  libLCE = await LibLCEWasm()
+
+  libLCE.printLibraryInfo()
+
   return mount(App, {
     target: document.getElementById('app')!
   })
 }
+
 // if we don't wait for initL10N we get locale loading race condition afaik
 initApp().then((mounted) => {
   app = mounted
 })
-
-// @ts-ignore Because it already IS esnext.
-export const libLCE = await LibLCEWasm()
-
-libLCE.printLibraryInfo()
 
 export default app
 

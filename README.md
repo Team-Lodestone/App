4565cb7eb81c8eb2c3cd8831aa8b887bb45c5980
Dexrn ZacAttack <me@dexrn.me>
2026-03-17 22:07:52 -0700

    chore: add license and other stuff

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lodestone.App
+The frontend for Project Lodestone.
+Uses [libLodestone](https://github.com/Team-Lodestone/libLodestone) internally.
+
+## Roadmap
+- [ ] Allow for loading a World, Level, maybe even individual regions and chunks.
+- [ ] Allow for exporting worlds, levels, chunks, etc
+  - [ ] Allow for exporting e.g. a Level as a World. 
+  - This is how we will achieve user facing world conversion.
--- src/gui/screens/AboutScreen.cpp
+++ src/gui/screens/AboutScreen.cpp
@@ -23,57 +23,57 @@
 #include "Lodestone.Common/LodestoneCommon.h"
 
 namespace lodestone::app::gui::screens {
-  AboutScreen::AboutScreen(LodestoneApp* app, QWidget* parent) : QWidget(parent), m_app(app) {
-    this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    AboutScreen::AboutScreen(LodestoneApp *app, QWidget *parent) : QWidget(parent), m_app(app) {
+        this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 
-    QVBoxLayout *outer = new QVBoxLayout(this);
+        const auto outer = new QVBoxLayout(this);
 
-    // LAYOUT INIT
-    this->m_layout = new QVBoxLayout();
-    this->m_layout->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
+        // LAYOUT INIT
+        this->m_layout = new QVBoxLayout();
+        this->m_layout->setAlignment(Qt::AlignHCenter | Qt::AlignTop);
 
-    this->m_layout->setContentsMargins(0, 0, 0, 0);
-    this->m_layout->setSpacing(0);
+        this->m_layout->setContentsMargins(0, 0, 0, 0);
+        this->m_layout->setSpacing(0);
 
-    outer->addLayout(this->m_layout);
+        outer->addLayout(this->m_layout);
 
-    //CHILDREN INIT
-    //add title
-    this->m_layout->addWidget(new widgets::TitleWidget());
+        //CHILDREN INIT
+        //add title
+        this->m_layout->addWidget(new widgets::TitleWidget());
 
-    //REGISTERED EXTENSIONS INIT
-    this->m_registeredExtensions = new QVBoxLayout();
-    connect(app, &LodestoneApp::extensionInitialized, this, &AboutScreen::onExtensionRegistered);
+        //REGISTERED EXTENSIONS INIT
+        this->m_registeredExtensions = new QVBoxLayout();
+        connect(app, &LodestoneApp::extensionInitialized, this, &AboutScreen::onExtensionRegistered);
 
-    QGroupBox *registeredExtensionsContainer = new QGroupBox("Registered Extensions", this);
-    registeredExtensionsContainer->setLayout(this->m_registeredExtensions);
+        const auto registeredExtensionsContainer = new QGroupBox("Registered Extensions", this);
+        registeredExtensionsContainer->setLayout(this->m_registeredExtensions);
 
-    this->m_layout->addWidget(registeredExtensionsContainer);
+        this->m_layout->addWidget(registeredExtensionsContainer);
 
-    //OUTER INIT
+        //OUTER INIT
 
-    const QIcon backIcon = QApplication::style()->standardIcon(QStyle::SP_ArrowLeft);
-    QPushButton *backButton = qt::variant::QPushButtonVariantFactory::createWithIcon(backIcon, {24, 24}, "Go back", this);
+        const QIcon backIcon = QApplication::style()->standardIcon(QStyle::SP_ArrowLeft);
+        QPushButton *backButton = qt::variant::QPushButtonVariantFactory::createWithIcon(backIcon, {24, 24}, "Go back", this);
 
-    backButton->connect(backButton, &QPushButton::clicked, this, &AboutScreen::onBackButtonClicked);
-    backButton->show();
-    backButton->move(5, 5);
+        QPushButton::connect(backButton, &QPushButton::clicked, this, &AboutScreen::onBackButtonClicked);
+        backButton->show();
+        backButton->move(5, 5);
 
-    QLabel *libVersionLabel = new QLabel("libLodestone v" + QString::fromUtf8(lodestone::common::lodestone_get_version_string()));
-    outer->addWidget(libVersionLabel, 0, Qt::AlignLeft | Qt::AlignBottom);
-  }
+        const auto libVersionLabel = new QLabel("libLodestone v" + QString::fromUtf8(lodestone::common::lodestone_get_version_string()));
+        outer->addWidget(libVersionLabel, 0, Qt::AlignLeft | Qt::AlignBottom);
+    }
 
-  void AboutScreen::onExtensionRegistered(const core::LodestoneExtension* ext) const {
-      this->m_registeredExtensions->addWidget(
-        new QLabel(
-          QString::fromStdString(
-            std::format("{} {}", ext->getIdentifier(), ext->getVersion().toString())
-          )
-        )
-      );
-  }
+    void AboutScreen::onExtensionRegistered(const core::LodestoneExtension *ext) const {
+        this->m_registeredExtensions->addWidget(
+            new QLabel(
+                QString::fromStdString(
+                    std::format("{} {}", ext->getIdentifier(), ext->getVersion().toString())
+                    )
+                )
+            );
+    }
 
-  void AboutScreen::onBackButtonClicked() {
-    this->m_app->window().switchToPreviousScreen();
-  }
+    void AboutScreen::onBackButtonClicked() {
+        this->m_app->window().switchToPreviousScreen();
+    }
 }

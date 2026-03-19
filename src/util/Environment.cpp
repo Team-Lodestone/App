64f6e4036a1891b926df925802686792606cd295
Dexrn ZacAttack <me@dexrn.me>
2026-03-18 23:35:28 -0700

    fix: "resolve" abi error (see: https://forum.qt.io/post/812515)

--- src/util/Environment.cpp
+++ src/util/Environment.cpp
@@ -11,15 +11,16 @@
 #include <qcoreapplication.h>
 
 namespace lodestone::app::util {
+    // HACK TO GET AROUND https://forum.qt.io/post/812515
     std::filesystem::path Environment::getStorageDirectory() {
-        return QStandardPaths::writableLocation(QStandardPaths::AppDataLocation).toStdString();
+        return QStandardPaths::writableLocation(QStandardPaths::AppDataLocation).toUtf8().data();
     }
 
     std::filesystem::path Environment::getConfigDirectory() {
-        return QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation).toStdString();
+        return QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation).toUtf8().data();
     }
 
     std::filesystem::path Environment::getCurrentDirectory() {
-        return QCoreApplication::applicationDirPath().toStdString();
+        return QCoreApplication::applicationDirPath().toUtf8().data();
     }
 }

fa6706f3680d23817b1595a74c7a763d1b67f018
Zero_DSRS_VX <24632178+PhoenixVX@users.noreply.github.com>
2026-03-19 21:03:08 -0600

    fix: Add 64x64 icon variants

--- rsrc/macos/generate_iconset.sh
+++ rsrc/macos/generate_iconset.sh
@@ -6,6 +6,8 @@ rsvg-convert -h 16 ../assets/icons/icon.svg > Lodestone.iconset/icon_16x16.png
 rsvg-convert -h 32 ../assets/icons/icon.svg > Lodestone.iconset/icon_16x16@2x.png
 rsvg-convert -h 32 ../assets/icons/icon.svg > Lodestone.iconset/icon_32x32.png
 rsvg-convert -h 64 ../assets/icons/icon.svg > Lodestone.iconset/icon_32x32@2x.png
+rsvg-convert -h 64 ../assets/icons/icon.svg > Lodestone.iconset/icon_64x64.png
+rsvg-convert -h 128 ../assets/icons/icon.svg > Lodestone.iconset/icon_64x64@2x.png
 rsvg-convert -h 128 ../assets/icons/icon.svg > Lodestone.iconset/icon_128x128.png
 rsvg-convert -h 256 ../assets/icons/icon.svg > Lodestone.iconset/icon_128x128@2x.png
 rsvg-convert -h 256 ../assets/icons/icon.svg > Lodestone.iconset/icon_256x256.png
@@ -14,3 +16,5 @@ rsvg-convert -h 512 ../assets/icons/icon.svg > Lodestone.iconset/icon_512x512.pn
 rsvg-convert -h 1024 ../assets/icons/icon.svg > Lodestone.iconset/icon_512x512@2x.png
 
 iconutil -c icns Lodestone.iconset
+mv Lodestone.iconset/* ../assets/icons/
+rm -rf Lodestone.iconset
